fb49911b68e2aa60d97072510e50ce80624e25a9
AniDrag <nikecoblak@gmail.com>
2025-09-08 17:15:54 +0200

    Small update

--- Assets/01_Scripts/Bees/BeeCore.cs
+++ Assets/01_Scripts/Bees/BeeCore.cs
@@ -2,15 +2,48 @@ using UnityEngine;
 
 public class BeeCore : Stats
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    //float xpToLevelMulti = 1;
+    float healthMulti = 1;
+    float staminaMulti = 1;
+    float magicMulti = 1;
+    float physicalDefMulti = 1;
+    float magicDefMulti = 1;
+    float statusDefMulti = 1;
+
+    int vit = 1;
+    int str = 1;
+    int dex = 1;
+    int agi = 1;
+    int mag = 1;
+
+    private Vector3 target;
+
+    #region Cobat Logic
+
+    #endregion
+    //----------------------------------------------
+    #region Traversal Logic
+    void GoToDestination()
     {
-        
+
+    }
+    #endregion
+    //----------------------------------------------
+    #region OffServer Calculations
+    
+
+    #endregion
+    //----------------------------------------------
+    #region Server Senders
+    void SendRequestCollectPollin()
+    {
+        // get location data and trigger traversal
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetDestination(Vector3 destination)
     {
-        
+        target = destination;
+        GoToDestination();
     }
+    #endregion
 }
